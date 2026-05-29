caee62da0315b634d05724723b77ccf9dedd264a
onkar18829 <119295557+onkar18829@users.noreply.github.com>
2026-05-30 00:04:13 +0530

    update model to gemma 4

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -72,7 +72,7 @@ export const createUserProject = async (req: Request, res: Response) => {
 
         // Enhance user prompt
         const promptEnhanceResponse = await openai.chat.completions.create({
-           model: 'google/gemma-3-27b-it:free',
+           model: 'openrouter.ai/google/gemma-3-27b-it',
             messages: [
                 {
                     role: 'system',
@@ -116,7 +116,7 @@ export const createUserProject = async (req: Request, res: Response) => {
 
         // Generate website code
         const codeGenerationResponse = await openai.chat.completions.create({
-            model: 'google/gemma-3-27b-it:free',
+            model: 'openrouter.ai/google/gemma-3-27b-it',
             messages: [
                 {
                     role: 'system',
